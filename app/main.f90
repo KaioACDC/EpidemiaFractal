49d6184f0d2e1f5f04bacafdbbf955e71f6fb8e7
Kaio <kaio.chagas@ufv.br>
2025-01-29 10:32:28 -0300

    Comentarios adicionados

--- app/main.f90
+++ app/main.f90
@@ -1,15 +1,18 @@
 program main
-  use sistema
+  use sistema !importando o modulo1.f90
     implicit none
 
-    real(8) :: delta_t(2) = [0.0, 50.0]
-    real(8) :: y0(4) = [0.7, 0.01, 0.0, 0.29]
-    real(8) :: t_inf = 0.5
-    real(8) :: t_rec = 0.1
-    integer :: pt = 1000
-    character(len=*), parameter :: arquivo_saida = 'dados.txt'
+    !parametros
+    real(8) :: delta_t(2) = [0.0, 150.0] !intervalo de tempo
+    real(8) :: y0(3) = [0.7, 0.01, 0.0] !condiçoes iniciais do sistema (s, i, r)
+    real(8) :: t_inf = 0.5 !taxa de infecção
+    real(8) :: t_rec = 0.1 !taxa de recuperação
+    integer :: pt = 1000 !números de pontos na simulação
+    character(len=*), parameter :: arquivo_saida = 'dados.txt' !arquivo de saida
 
-    call kutta_sistema(delta_t, y0, t_inf, t_rec, pt, arquivo_saida)
-
+    !resolvendo o sistema com Runge-Kutta
+    call kutta_sistema(delta_t, y0, t_inf, t_rec, pt, arquivo_saida) !
+    
+    !Indicando que os dados foram salvos
     print*, "Dados salvos em", arquivo_saida
 end program main
