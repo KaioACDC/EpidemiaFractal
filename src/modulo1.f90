865c2f39fb5a0c66f9a9e8cdd1bda24829c7fc44
Kaio <kaio.chagas@ufv.br>
2025-01-29 01:02:33 -0300

    Adicionando os primeiro parametros

--- src/modulo1.f90
+++ src/modulo1.f90
@@ -1,2 +1,13 @@
 module sistema 
     implicit none
+
+contains
+    subroutine sistema_e_d(t, y, dydt, t_inf, t_rec)
+
+        real(8), intent(in) :: t
+        real(8), intent(in) :: t_inf
+        real(8), intent(in) :: t_rec
+        real(8), intent(in) :: y(4)
+
+        
+        real(8), intent(out) :: dydt(4)
