124558e673580fab11619b3e9fdf36419b98d81d
Kaio <kaio.chagas@ufv.br>
2025-01-29 08:00:46 -0300

    Adicionando os vizinhos à lista de infectados

--- fractal.py
+++ fractal.py
@@ -7,3 +7,14 @@ rede = np.zeros((L, L), dtype=int)
 
 centro = L // 2 
 rede[centro, centro] = 1
+
+epidemia = []
+
+for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+    x, y = centro + dx, centro + dy
+    if 0 <= x < L and 0 <= y < L: 
+        epidemia.append((x, y))
+
+plt.ion()
+fig, ax = plt.subplot()
+im 
