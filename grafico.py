704141b66902110907f8cf670a730c65cd6a4486
Kaio <kaio.chagas@ufv.br>
2025-01-29 02:01:56 -0300

    Definindo parametros

--- grafico.py
+++ grafico.py
@@ -2,3 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dados = np.loadtxt('dados.txt', skiprows=1)
+
+tempo = dados[:, 0]
+s = dados[:, 1]
+i = dados[:, 2]
+r = dados[:, 3]
+n = dados[:, 4]
