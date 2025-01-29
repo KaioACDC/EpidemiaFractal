27e6ab60f903321c6efb130a1a72c6cac1776c3a
Kaio <kaio.chagas@ufv.br>
2025-01-29 02:02:29 -0300

    Definições do grafico

--- grafico.py
+++ grafico.py
@@ -8,3 +8,15 @@ s = dados[:, 1]
 i = dados[:, 2]
 r = dados[:, 3]
 n = dados[:, 4]
+
+plt.figure(figsize=(10, 6))
+plt.plot(tempo, s, label='Suscetíveis', color='blue')
+plt.plot(tempo, i, label='Infectados', color='red')
+plt.plot(tempo, r, label='Recuperados', color='green')
+plt.plot(tempo, n, label='Não infectados', color='purple', linestyle='--')
+plt.title("Epidemia")
+plt.xlabel("Tempo")
+plt.ylabel('Fração da população')
+plt.legend()
+plt.grid()
+plt.show()
