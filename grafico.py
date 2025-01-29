7939d249f9cb057978bf7b88665d46ec52f7fb54
Kaio <kaio.chagas@ufv.br>
2025-01-29 10:39:40 -0300

    Comentarios adicionados

--- grafico.py
+++ grafico.py
@@ -1,22 +1,31 @@
+#bibliotecas
 import numpy as np
 import matplotlib.pyplot as plt
 
+#carrega os dados gerado pelo nosso programa no fortran
 dados = np.loadtxt('dados.txt', skiprows=1)
 
-tempo = dados[:, 0]
-s = dados[:, 1]
-i = dados[:, 2]
-r = dados[:, 3]
-n = dados[:, 4]
+#criando uma matriz com colunas individuais
+tempo = dados[:, 0] #tempo 
+s = dados[:, 1] #sucetiveis
+i = dados[:, 2] #infectados
+r = dados[:, 3] #recuperada
 
-plt.figure(figsize=(10, 6))
+
+plt.figure(figsize=(10, 6)) #tamanho do grafico
+
+#curvas de evolução da população
 plt.plot(tempo, s, label='Suscetíveis', color='blue')
 plt.plot(tempo, i, label='Infectados', color='red')
 plt.plot(tempo, r, label='Recuperados', color='green')
-plt.plot(tempo, n, label='Não infectados', color='purple', linestyle='--')
-plt.title("Epidemia")
+
+
+plt.title("Epidemia") #nome do grafico
+
+#nome dos eixos
 plt.xlabel("Tempo")
 plt.ylabel('Fração da população')
-plt.legend()
-plt.grid()
-plt.show()
+
+plt.legend() #legenda para as curvas
+plt.grid() #adiciona uma grade
+plt.show() #para exibir o grafico
