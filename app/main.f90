d3e0bc0f53d99771ce9c4ecb021ce0915708c734
Kaio <kaio.chagas@ufv.br>
2025-01-29 01:41:24 -0300

    Definindo parametros

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,10 @@
 program main
-  use trabalho, only: say_hello
-  implicit none
+  use sistema
+    implicit none
 
-  call say_hello()
-end program main
+    real(8) :: delta_t(2) = [0.0, 50.0]
+    real(8) :: y0(4) = [0.7, 0.01, 0.0, 0.29]
+    real(8) :: t_inf = 0.5
+    real(8) :: t_rec = 0.1
+    integer :: pt = 1000
+    character(len=*), parameter :: arquivo_saida = 'dados.txt'
